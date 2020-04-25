19ef84ee3c0996b699ed7066c237bdde3c6e8344
Julian <julian.ibarra007@gmail.com>
2020-04-25 10:46:21 -0500

    Updates

---
+++ precache-manifest.58cd30c12d6e0e82a2c32ec6b2595b59.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac318b06c9962aa3485fb88f593c1d1b",
+    "url": "/Krooppa/index.html"
+  },
+  {
+    "revision": "c3211a8e2138b3bb2d00",
+    "url": "/Krooppa/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "f6c5504d9503622a5a8c",
+    "url": "/Krooppa/static/css/main.519a88ba.chunk.css"
+  },
+  {
+    "revision": "c3211a8e2138b3bb2d00",
+    "url": "/Krooppa/static/js/2.f723cd75.chunk.js"
+  },
+  {
+    "revision": "ccc502121842bd2ec2d999aa7d823359",
+    "url": "/Krooppa/static/js/2.f723cd75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c5504d9503622a5a8c",
+    "url": "/Krooppa/static/js/main.e8db9a57.chunk.js"
+  },
+  {
+    "revision": "9c2f232c4fb4aa62e69c",
+    "url": "/Krooppa/static/js/runtime-main.7f403d2e.js"
+  },
+  {
+    "revision": "bf7355bace7a3ec77c064c2a2c70750e",
+    "url": "/Krooppa/static/media/FUTURAMC.bf7355ba.TTF"
+  },
+  {
+    "revision": "c8ec09a0560c409b7d9c58891b327510",
+    "url": "/Krooppa/static/media/FUTURAMC.c8ec09a0.otf"
+  },
+  {
+    "revision": "e374ae720b14326ea930e037a7086df6",
+    "url": "/Krooppa/static/media/app-store.e374ae72.svg"
+  },
+  {
+    "revision": "18e1903dde6bb8c5cf483948b93ea579",
+    "url": "/Krooppa/static/media/backgnd3.18e1903d.png"
+  },
+  {
+    "revision": "acbb32e0e2fc5b5219c4cd9a74fec3ad",
+    "url": "/Krooppa/static/media/backgnd3s.acbb32e0.png"
+  },
+  {
+    "revision": "c68e1c2faea7c511f7f5093e1997520f",
+    "url": "/Krooppa/static/media/google-play.c68e1c2f.svg"
+  },
+  {
+    "revision": "3edfe43f1b43d7d05a8d47efc3a2450f",
+    "url": "/Krooppa/static/media/logo-line.3edfe43f.svg"
+  },
+  {
+    "revision": "e55aaab5ce00402cb0438139adbac59e",
+    "url": "/Krooppa/static/media/phonec8.e55aaab5.png"
+  }
+]);
